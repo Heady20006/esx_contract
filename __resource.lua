e542fcd0d0302e45756a9fb654a4ec3eab964533
Fred Richter <45440493+Heady20006@users.noreply.github.com>
2019-03-20 18:22:39 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Contract'
+
+version '1.0.0'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'config.lua',
+	'client/main.lua'
+}
---
+++ contract-sql.sql
@@ -0,0 +1,2 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES 
+('contract', 'Kaufvertrag', '5', '0', '1');
