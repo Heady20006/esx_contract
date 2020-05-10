7160520ce38b061d6cc9a48a5f3247219e1c43e8
dracoling <dracoling@gmail.com>
2020-05-10 17:39:35 -0400

    Load all available locales

--- __resource.lua
+++ __resource.lua
@@ -7,14 +7,20 @@ version '1.0.0'
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
+	'locales/br.lua',
 	'locales/de.lua',
+	'locales/en.lua',
+	'locales/tr.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
+	'locales/br.lua',
 	'locales/de.lua',
+	'locales/en.lua',
+	'locales/tr.lua',
 	'config.lua',
 	'client/main.lua'
 }
